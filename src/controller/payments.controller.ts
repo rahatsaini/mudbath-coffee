75661eec2a5c156bf55df68a8be884d9e7152214
rahat saini <rahat.saini@finity.com.au>
2022-01-14 01:09:33 +1100

    total what user paid

--- src/controller/payments.controller.ts
+++ src/controller/payments.controller.ts
@@ -1,17 +1,37 @@
 
 import { Payment } from '../data/models/payment';
-import { PriceList } from '../data/models/priceList';
 import data from '../data/payments.json';
-
+import * as _ from "lodash"
 export class PaymentController {
     private payments: Payment[] = [];
+    private totalPayments: Payment[] | undefined = [] || undefined;
     constructor(){
     }
 
     readPayments(){
       this.payments = data;
+      this.calculateTotalForEachUser();
     }
    
+    calculateTotalForEachUser(){
+      let res: any =[];
+      this.Payments.forEach(function(item, index) {
+        if (res.length === 0 
+            || !res.some(function(elem: any) {return elem.user === item.user}) ) {
+          res.push(item)
+        } else {
+          for (var i = 0; i < res.length; i++) {
+            if (res[i]["user"] === item["user"] 
+                && (res[i]["amount"] !== 1 && item["amount"] !== 1)) {
+              res[i]["amount"] += item["amount"]
+            }
+          }
+        }
+      });
+      this.totalPayments = res;
+  
+}
+
     get Payments(){return this.payments;}
 
 }
