83d7f1143999d41b410dfa7fd0d3a085bc24e8a4
rahat saini <rahat.saini@finity.com.au>
2022-01-14 14:46:35 +1100

    updated jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports ={
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    testMatch: [
+        '<rootDir>/src/**/*.test.js',
+        '<rootDir>/src/**/*.test.jsx',
+    ],
 };
